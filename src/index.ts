fad81d5cdb0e9cc3b8c5cafd14f9cb7e5ec34928
AlexJester147 <vardy2016@lenta.ru>
2022-02-04 00:11:20 +0300

    feat : add basic structure of start page

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,3 @@
 import { renderStartPage } from "./start-page/start-page";
 
-
 renderStartPage();
--- src/start-page/start-page.ts
+++ src/start-page/start-page.ts
@@ -1,9 +1,26 @@
 import '../start-page/start-page.css';
 
 export function renderStartPage() {
- const startPage = `
- <h1> ГЛАВНАЯ СТРАНИЦА</h1>
+  const startPage = `
+  <header class="header">
+  <nav class="nav">
+      <div class="app-name">RSLang</div>
+      <div class="register-sign__block">
+          <button class="register-open__button">Register</button>
+          <button class="sign-open__button">Sign in</button>
+      </div>
+  </nav>
+</header>
+
+<main class="main">
+  <section class="games-button__wrap">
+      <div class="game1">Game1</div>
+      <div class="game2">Game2</div>
+  </section>
+</main>
+
+<footer class="footer"></footer>
  `;
- const body = document.querySelector('.body') as HTMLElement;
- body.innerHTML = startPage;   
+  const body = document.querySelector('.body') as HTMLElement;
+  body.innerHTML = startPage;
 }
