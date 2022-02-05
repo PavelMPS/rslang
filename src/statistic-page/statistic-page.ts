cabf68dc1c62a8747d0498a581563ee1bb29633d
AlexJester147 <vardy2016@lenta.ru>
2022-02-05 23:00:34 +0300

    feat : redesigh statistics page

--- src/statistic-page/statistic-page.ts
+++ src/statistic-page/statistic-page.ts
@@ -3,47 +3,75 @@ import '../statistic-page/statistic-page.css';
 
 export async function renderStatisticPage(): Promise<void> {
     const content: string = `
-        <div class="games-statistic-title-container">
-            <h2 class="games-statistic-title">Mini-games statistic:</h2>
-        </div>
+    <div class="games-statistic-title-container">
+        <h2 class="games-statistic-title">Mini-games statistic:</h2>
+    </div>
 
-        <div class="games-statistic-container">
-            <div class="game-sprint-statistic-container">
-                <div class="game-sprint">
-                    <div class="game-sprint-title">
-                        <h3>SPRINT</h3>
-                    </div>
-                    <div class="sprint-newwords-stat">New words count: 0 words</div>
-                    <div class="sprint-right-answer-stat">Right answers count: 0%</div>
-                    <div class="sprint-longest-serie-stat">The longest serie: 0 words</div>
+<div class="games-statistic-container">
+    <div class="statistics-container">
+        <div class="game-sprint">
+            <div class="game-sprint-title">
+                <h3>Sprint</h3>
+            </div>
+            <div class="stats-block__wrap">
+                <div class="stats__description-block">
+                    <div class="stats__description">New words count</div>
+                    <div class="stats__description">Right answers</div>
+                    <div class="stats__description">The longest serie</div>
+                </div>
+                <div class="stats__info-block">
+                    <div class="stats__info">0 words</div>
+                    <div class="stats__info">0%</div>
+                    <div class="stats__info">0 words</div>
                 </div>
             </div>
-            <div class="game-audiochallenge-statistic-container">
-                <div class="game-audiochallenge">
-                     <div>
-                        <h3 class="game-audiochallenge-title">AUDIOCHALLENGE</h3>
-                    </div>
-                    <div class="audiochallenge-newwords-stat">New words count: 0 words</div>
-                    <div class="audiochallenge-right-answer-stat">Right answers: 0%</div>
-                    <div class="audiochallenge-longest-serie-stat">The longest serie: 0 words</div>
+        </div>
+    </div>
+
+    <div class="statistics-container">
+        <div class="game-audiochallenge">
+            <div>
+                <h3 class="game-audiochallenge-title">Audio Challenge</h3>
+            </div>
+            <div class="stats-block__wrap">
+                <div class="stats__description-block">
+                    <div class="stats__description">New words count</div>
+                    <div class="stats__description">Right answers</div>
+                    <div class="stats__description">The longest serie</div>
+                </div>
+                <div class="stats__info-block">
+                    <div class="stats__info">0 words</div>
+                    <div class="stats__info">0%</div>
+                    <div class="stats__info">0 words</div>
                 </div>
             </div>
-
         </div>
+    </div>
 
-        <div class="words-statistic-title-container">
-            <h2 class="words-statistic-title">Words statistic:</h2>
-        </div>
+</div>
 
-        <div class="words-statistic-container">
-            <div class="new-words-count">New words count: 0 words</div>
-            <div class="learn-words-count">Learned words count: 0 words</div>
-            <div class="right-answers-count">Right answers: 0%</div>
-        </div>
-        <button class="btn home-btn">Home</div>
+<div class="words-statistic-title-container">
+    <h2 class="words-statistic-title">Words statistic:</h2>
+</div>
+<div class="statistics-container">
+<div class="stats-block__wrap">
+    <div class="stats__description-block">
+        <div class="stats__description">New words count</div>
+        <div class="stats__description">Right answers</div>
+        <div class="stats__description">Learned words count</div>
+    </div>
+    <div class="stats__info-block">
+        <div class="stats__info">0 words</div>
+        <div class="stats__info">0%</div>
+        <div class="stats__info">0 words</div>
+    </div>
+</div>
+</div>
+
+<button class="btn home-btn">Home</button>
     `;
 
-    const main = document.querySelector('.main') as HTMLElement;  
+    const main = document.querySelector('.main') as HTMLElement;
     main.innerHTML = content;
     await backToHome();
 }
