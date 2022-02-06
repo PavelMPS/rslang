95595f97868ef3ca23efe738f6813283f05c7cd2
AlexJester147 <vardy2016@lenta.ru>
2022-02-06 19:03:05 +0300

    feat : add stickers to stats page

--- src/statistic-page/statistic-page.ts
+++ src/statistic-page/statistic-page.ts
@@ -7,69 +7,72 @@ export async function renderStatisticPage(): Promise<void> {
         <h2 class="games-statistic-title">Mini-games statistic:</h2>
     </div>
 
-<div class="games-statistic-container">
-    <div class="statistics-container">
-        <div class="game-sprint">
-            <div class="game-sprint-title">
-                <h3>Sprint</h3>
-            </div>
-            <div class="stats-block__wrap">
+    <div class="games-statistic-container">
+        <div class="statistics-container">
+            <div class="game-sprint">
+                <div class="game-sprint-title">
+                    <h3>Sprint</h3>
+                </div>
+                <div class="stats-block__wrap">
+                <img class="stats-sticker__img" src="./assets/analysis.png"> 
                 <div class="stats__description-block">
-                    <div class="stats__description">New words count</div>
-                    <div class="stats__description">Right answers</div>
-                    <div class="stats__description">The longest serie</div>
+                    <div class="stats__description">New words</div> 
+                    <div class="stats__description">Right answers</div> 
+                    <div class="stats__description">The longest serie</div> 
                 </div>
                 <div class="stats__info-block">
-                    <div class="stats__info">0 words</div>
-                    <div class="stats__info">0%</div>
-                    <div class="stats__info">0 words</div>
+                    <div class="stats__info" class="sprint-new__words-count">0 words</div>
+                    <div class="stats__info" class="sprint-right__words-count">0%</div>
+                    <div class="stats__info" class="sprint-longest__words-serie">0 words</div>
+                </div>
                 </div>
             </div>
         </div>
-    </div>
-
-    <div class="statistics-container">
-        <div class="game-audiochallenge">
-            <div>
-                <h3 class="game-audiochallenge-title">Audio Challenge</h3>
-            </div>
-            <div class="stats-block__wrap">
+        <div class="statistics-container">
+            <div class="game-audiochallenge">
+                 <div>
+                    <h3 class="game-audiochallenge-title">Audio Challenge</h3>
+                </div>
+                <div class="stats-block__wrap">
+                <img class="stats-sticker__img" src="./assets/pie.png"> 
                 <div class="stats__description-block">
-                    <div class="stats__description">New words count</div>
-                    <div class="stats__description">Right answers</div>
-                    <div class="stats__description">The longest serie</div>
+                    <div class="stats__description">New words</div> 
+                    <div class="stats__description">Right answers</div> 
+                    <div class="stats__description">The longest serie</div> 
                 </div>
                 <div class="stats__info-block">
-                    <div class="stats__info">0 words</div>
-                    <div class="stats__info">0%</div>
-                    <div class="stats__info">0 words</div>
+                    <div class="stats__info" class="audiochallenge-new__words-count">0 words</div>
+                    <div class="stats__info" class="audiochallenge-right__words-count">0%</div>
+                    <div class="stats__info" class="audiochallenge-longest__words-serie">0 words</div>
+                </div>
                 </div>
             </div>
         </div>
+
     </div>
 
-</div>
-
-<div class="words-statistic-title-container">
-    <h2 class="words-statistic-title">Words statistic:</h2>
-</div>
-<div class="statistics-container">
-<div class="stats-block__wrap">
-    <div class="stats__description-block">
-        <div class="stats__description">New words count</div>
-        <div class="stats__description">Right answers</div>
-        <div class="stats__description">Learned words count</div>
+    <div class="words-statistic-title-container">
+        <h2 class="words-statistic-title">Words statistic:</h2>
     </div>
-    <div class="stats__info-block">
-        <div class="stats__info">0 words</div>
-        <div class="stats__info">0%</div>
-        <div class="stats__info">0 words</div>
+    <div class="stats-block__wrap">
+    <div class="statistics-container">
+    <div class="stats-block__wrap">
+    <img class="stats-sticker__img" src="./assets/search.png"> 
+        <div class="stats__description-block">
+            <div class="stats__description">New words count</div> 
+            <div class="stats__description">Right answers</div> 
+            <div class="stats__description">Learned words count</div> 
+        </div>
+        <div class="stats__info-block">
+            <div class="stats__info" class="everyday-new__words-count">0 words</div>
+            <div class="stats__info" class="everyday-learn__words-count">0 words</div>
+            <div class="stats__info" class="everyday-right__words-count">0%</div>
+       </div>
+       </div>
     </div>
-</div>
-</div>
-
-<button class="btn home-btn">Home</button>
-    `;
+    </div>
+    <button class="btn home-btn">Home</div>
+`;
 
     const main = document.querySelector('.main') as HTMLElement;
     main.innerHTML = content;
