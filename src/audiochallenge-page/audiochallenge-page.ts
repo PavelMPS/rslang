50baed08eb47a6b87739825024cc0d8f86ffde06
Veronika Yashchenkova <ronkaarizona@gmail.com>
2022-02-06 21:25:30 +0300

    feat: add basic structure of audiochallenge page

--- src/audiochallenge-page/audiochallenge-page.ts
+++ src/audiochallenge-page/audiochallenge-page.ts
@@ -3,7 +3,33 @@ import '../audiochallenge-page/audiochallenge-page.css';
 export async function renderAudiochallengePage() {
   console.log('audiochallenge');
   //TODO function
+  const content: string = `    <div class="audiochallenge-container">
+      <div class="hearts-container">
+        <div class="heart"></div>
+        <div class="heart"></div>
+        <div class="heart"></div>
+        <div class="heart"></div>
+        <div class="heart broken"></div>
+      </div>
+      <div class="listen-btn"></div>
+      <div class="btns-container">
+        <div class="answers-btn-container">
+          <div class="answers-btn" id="one">one</div>
+          <div class="answers-btn" id="two">two</div>
+          <div class="answers-btn" id="three">three</div>
+          <div class="answers-btn" id="four">four</div>
+          <div class="answers-btn" id="five">five</div>
+        </div>
+        <div class="next-question-btn">NEXT</div>
+      </div>
+    </div>`;
+
+  const main = document.querySelector('.main') as HTMLElement;
+  main.innerHTML = content;
 }
+
+//в качестве переменной game в функцию передается название игры 'audiochallenge'/'sprint' от которой зависит текст, котик, листенер
+//реализовано получение номера группы при выборе сложности
 
 export async function renderGroupSelectionPage(game: string): Promise<void> {
     const content: string = ` <div class="group-select-page">
@@ -18,12 +44,12 @@ export async function renderGroupSelectionPage(game: string): Promise<void> {
       <div class="game-difficult-container">
           <h4 class="difficult-subtitle">Choose the difficult of the game:</h4>
           <div class="difficult-buttons-container">
-              <button class="btn difficult-btn" id="difficult-btn-1">I</button>
-              <button class="btn difficult-btn" id="difficult-btn-2">II</button>
-              <button class="btn difficult-btn" id="difficult-btn-3">III</button>
-              <button class="btn difficult-btn" id="difficult-btn-4">IV</button>
-              <button class="btn difficult-btn" id="difficult-btn-5">V</button>
-              <button class="btn difficult-btn" id="difficult-btn-6">VI</button>
+              <button class="btn difficult-btn" id="difficult-btn-1" data-group="0">I</button>
+              <button class="btn difficult-btn" id="difficult-btn-2" data-group="1">II</button>
+              <button class="btn difficult-btn" id="difficult-btn-3" data-group="2">III</button>
+              <button class="btn difficult-btn" id="difficult-btn-4" data-group="3">IV</button>
+              <button class="btn difficult-btn" id="difficult-btn-5" data-group="4">V</button>
+              <button class="btn difficult-btn" id="difficult-btn-6" data-group="5">VI</button>
           </div>
       </div>
       <div class="btn start-btn">Let's start!</div>
@@ -50,13 +76,29 @@ export async function renderGroupSelectionPage(game: string): Promise<void> {
           <li>Use the Enter key for a hint or to move to the next word.</li>
         </ul>`;
 
+    const startBTN: HTMLElement = main.querySelector('.start-btn') as HTMLElement;
+
+    const difficultBTNs: NodeListOf<HTMLElement> = main.querySelectorAll('.difficult-btn') as NodeListOf<HTMLElement>;
+
+    let group: string|undefined = '0';
+
+    difficultBTNs.forEach((difficultBTN: HTMLElement): void => {
+      difficultBTN.addEventListener('click', () => {
+        difficultBTNs.forEach((btn: HTMLElement) => {
+          btn.classList.remove('active');
+        })
+        difficultBTN.classList.add('active');
+        group = difficultBTN.dataset.group;
+      })
+    })
+
     if (game === 'audiochallenge') {
       title.innerHTML = audiochallengeTitle;
       description.innerHTML = audiochallengeDescription;
+      startBTN.addEventListener('click', renderAudiochallengePage);
     } else if (game === 'sprint') {
       title.innerHTML = sprintTitle;
       description.innerHTML = sprintDescription;
+      //запускаем игру спринт
     }
-
-    await renderAudiochallengePage();
 }
