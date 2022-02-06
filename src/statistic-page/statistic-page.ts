cb6bf0be23079be4360aba4c658fa2f722390e0c
AlexJester147 <vardy2016@lenta.ru>
2022-02-06 19:48:14 +0300

    feat : stats page

--- src/statistic-page/statistic-page.ts
+++ src/statistic-page/statistic-page.ts
@@ -3,18 +3,17 @@ import '../statistic-page/statistic-page.css';
 
 export async function renderStatisticPage(): Promise<void> {
     const content: string = `
-    <div class="games-statistic-title-container">
-        <h2 class="games-statistic-title">Mini-games statistic:</h2>
+    <div class="statistic-page__title">
+        <h2 class="games-statistic-title">Statistics</h2>
     </div>
 
     <div class="games-statistic-container">
         <div class="statistics-container">
-            <div class="game-sprint">
-                <div class="game-sprint-title">
-                    <h3>Sprint</h3>
+                <div class="game-title">
+                    <h3>Sprint game</h3>
                 </div>
                 <div class="stats-block__wrap">
-                <img class="stats-sticker__img" src="./assets/analysis.png"> 
+                    <img class="stats-sticker__img" src="./assets/analysis.png"> 
                 <div class="stats__description-block">
                     <div class="stats__description">New words</div> 
                     <div class="stats__description">Right answers</div> 
@@ -25,13 +24,12 @@ export async function renderStatisticPage(): Promise<void> {
                     <div class="stats__info" class="sprint-right__words-count">0%</div>
                     <div class="stats__info" class="sprint-longest__words-serie">0 words</div>
                 </div>
-                </div>
             </div>
         </div>
+
         <div class="statistics-container">
-            <div class="game-audiochallenge">
-                 <div>
-                    <h3 class="game-audiochallenge-title">Audio Challenge</h3>
+                 <div class="game-title">
+                    <h3 class="game-title">Audio Challenge</h3>
                 </div>
                 <div class="stats-block__wrap">
                 <img class="stats-sticker__img" src="./assets/pie.png"> 
@@ -45,17 +43,13 @@ export async function renderStatisticPage(): Promise<void> {
                     <div class="stats__info" class="audiochallenge-right__words-count">0%</div>
                     <div class="stats__info" class="audiochallenge-longest__words-serie">0 words</div>
                 </div>
-                </div>
             </div>
         </div>
 
-    </div>
-
-    <div class="words-statistic-title-container">
-        <h2 class="words-statistic-title">Words statistic:</h2>
-    </div>
-    <div class="stats-block__wrap">
     <div class="statistics-container">
+        <div class="game-title">
+            <h3 class="game-title">Overall progress</h3>
+        </div>
     <div class="stats-block__wrap">
     <img class="stats-sticker__img" src="./assets/search.png"> 
         <div class="stats__description-block">
@@ -68,7 +62,7 @@ export async function renderStatisticPage(): Promise<void> {
             <div class="stats__info" class="everyday-learn__words-count">0 words</div>
             <div class="stats__info" class="everyday-right__words-count">0%</div>
        </div>
-       </div>
+    </div>
     </div>
     </div>
     <button class="btn home-btn">Home</div>
