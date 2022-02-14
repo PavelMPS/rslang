187ff1562acd6272eb52abea3e2e114f4728be55
AlexJester147 <vardy2016@lenta.ru>
2022-02-14 12:18:54 +0300

    feat : redesign authorization block

--- src/listen/authorization-listen.ts
+++ src/listen/authorization-listen.ts
@@ -1,40 +1,72 @@
-import { renderRegistrationBlock, renderSignBlock, createUser, loginUser } from "../authorization-block/authorization";
+import { renderAuthorizationBlock, renderRegistrationBlock, renderSignBlock, createUser, loginUser } from "../authorization-block/authorization";
 
 export async function authorizationListen(): Promise<void> {
-  const registerOpenButton = document.querySelector('.register-open__button') as HTMLElement;
-  const signOpenButton = document.querySelector('.sign-open__button') as HTMLElement;
 
-  const registerButtonsArray: Array<HTMLElement> = [registerOpenButton, signOpenButton];
+  const authorizationOpenButton = document.querySelector('.authorization-open__button') as HTMLElement;
+  const authorizationBlock = document.querySelector('.authorization-block') as HTMLElement;
+  authorizationBlock.dataset.open = 'false';
 
-  async function closeAuthorizationForms(): Promise<void> {
-    const closeForm = document.querySelector<HTMLElement>('.close-form');
-    closeForm?.addEventListener('click', (): void => {
-      (closeForm.parentNode?.parentNode as HTMLElement).innerHTML = ``;
-    });
-  }
+  authorizationOpenButton.addEventListener('click', (): void => {
 
-  registerOpenButton.addEventListener('click', (): void => {
-    registerButtonsArray.forEach(elem => (elem.nextElementSibling as HTMLElement).innerHTML = ``);
+    renderAuthorizationBlock();
     renderRegistrationBlock();
-    closeAuthorizationForms();
-    const registerSubmit = document.querySelector('.register-submit') as HTMLElement;
-    registerSubmit.addEventListener('click', () => {
-      const registerName = document.querySelector('#register-name') as HTMLInputElement;
-      const registerEmail = document.querySelector('#register-email') as HTMLInputElement;
-      const registerPassword = document.querySelector('#register-password') as HTMLInputElement;
-      createUser({ "name": `${registerName.value}`, "email": `${registerEmail.value}`, "password": `${registerPassword.value}` });
-    });
-  });
 
-  signOpenButton.addEventListener('click', (): void => {
-    registerButtonsArray.forEach(elem => (elem.nextElementSibling as HTMLElement).innerHTML = ``);
-    renderSignBlock();
-    closeAuthorizationForms();
-    const signSubmit = document.querySelector('.sign-submit') as HTMLElement;
-    signSubmit.addEventListener('click', () => {
-      const signEmail = document.querySelector('#sign-email') as HTMLInputElement;
-      const signPassword = document.querySelector('#sign-password') as HTMLInputElement;
-      loginUser({ "email": `${signEmail.value}`, "password": `${signPassword.value}` });
-    });
+    if (authorizationBlock.dataset.open == 'true') {
+      authorizationBlock.dataset.open = 'false';
+      authorizationBlock.innerHTML = '';
+    } else {
+      authorizationBlock.dataset.open = 'true';
+      renderAuthorizationBlock();
+      renderRegistrationBlock();
+      switchAuthorizeBlock([document.querySelector('.register-open__button') as HTMLElement, document.querySelector('.sign-open__button') as HTMLElement]);
+      signSubmitCall(document.querySelector('.register-block') as HTMLElement);
+      registerSubmitCall(document.querySelector('.sign-block') as HTMLElement);
+    }
+
+    function switchAuthorizeBlock(args: Array<HTMLElement>): void {
+      args[0].classList.add('active');
+      args.forEach((element: Element) => {
+        element.addEventListener('click', (): void => {
+          args.forEach((element: Element) => {
+            element.classList.remove('active');
+          });
+          element.classList.add('active');
+        });
+      });
+    };
+
+    function sendRegistrationInfo(): void {
+      const registerSubmit = document.querySelector('.register-submit') as HTMLInputElement;
+      registerSubmit.addEventListener('click', (): void => {
+        const registerName = document.querySelector('#register-name') as HTMLInputElement;
+        const registerEmail = document.querySelector('#register-email') as HTMLInputElement;
+        const registerPassword = document.querySelector('#register-password') as HTMLInputElement;
+        createUser({ "name": `${registerName.value}`, "email": `${registerEmail.value}`, "password": `${registerPassword.value}` });
+      });
+    }
+
+    function registerSubmitCall(element: HTMLElement): void {
+      sendRegistrationInfo();
+      const registerOpenButton = document.querySelector('.register-open__button') as HTMLElement;
+      registerOpenButton.addEventListener('click', (): void => {
+        element.innerHTML = '';
+        renderRegistrationBlock();
+        sendRegistrationInfo();
+      });
+    };
+
+    function signSubmitCall(element: HTMLElement): void {
+      const signOpenButton = document.querySelector('.sign-open__button') as HTMLElement;
+      signOpenButton.addEventListener('click', (): void => {
+        element.innerHTML = '';
+        renderSignBlock();
+        const signSubmit = document.querySelector('.sign-submit') as HTMLInputElement;
+        signSubmit.addEventListener('click', (): void => {
+          const signEmail = document.querySelector('#sign-email') as HTMLInputElement;
+          const signPassword = document.querySelector('#sign-password') as HTMLInputElement;
+          loginUser({ "email": `${signEmail.value}`, "password": `${signPassword.value}` });
+        });
+      });
+    };
   });
 };
