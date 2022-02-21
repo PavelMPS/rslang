1215f3e5eda78360b37cd40570f64dc2eb861262
AlexJester147 <vardy2016@lenta.ru>
2022-02-21 16:11:37 +0300

    feat : add type of RegExp variable

--- src/constants/auth.ts
+++ src/constants/auth.ts
@@ -4,4 +4,4 @@ export const notRegisteredEmailText = 'This email is not registered' as string;
 export const writeEmailCaption = 'Please, write correct email' as string;
 export const signSuccessText = 'Success!' as string;
 export const signPasswordEmailError = 'Incorrect e-mail or password' as string;
-export let reg = /^([A-Za-z0-9_\-\.])+\@([A-Za-z0-9_\-\.])+\.([A-Za-z]{2,4})$/;
+export let reg = /^([A-Za-z0-9_\-\.])+\@([A-Za-z0-9_\-\.])+\.([A-Za-z]{2,4})$/ as RegExp;
